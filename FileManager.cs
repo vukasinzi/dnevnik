7b42e4fc8c1c5da599fe0750cd49e610d2036f11
Vukasin <vukasinzivaljevic13@gmail.com>
2026-03-15 18:19:55 +0100

    brisanje izmena i cuvanje novih unosa

--- FileManager.cs
+++ FileManager.cs
@@ -3,7 +3,10 @@ using MessagePack;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
 using System.Text;
+using System.Text.Json;
 
 namespace journal
 {
@@ -19,7 +22,11 @@ namespace journal
                 return instance;
             }
         }
-
+        private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        
         public bool upisiUnos(string putanja, Unos unos)
         {
             try
@@ -29,7 +36,7 @@ namespace journal
                     prethodniUnosi = new();
                 prethodniUnosi.Insert(0,unos);
                 using FileStream fs = new FileStream(putanja, FileMode.Create, FileAccess.Write);
-                MessagePackSerializer.Serialize<List<Unos>>(fs, prethodniUnosi);
+                JsonSerializer.Serialize(fs, prethodniUnosi,_jsonOptions);
             }
             catch (Exception x)
             {
@@ -42,7 +49,7 @@ namespace journal
             if (!File.Exists(putanja))
                 return null;
             using FileStream fs = new FileStream(putanja, FileMode.Open, FileAccess.Read);
-            return MessagePackSerializer.Deserialize<List<Unos>>(fs);
+            return JsonSerializer.Deserialize<List<Unos>>(fs,_jsonOptions);
 
             
         }
@@ -60,5 +67,43 @@ namespace journal
 
         }
 
+       
+     
+        internal void sacuvajUnos(string putanja, int index,Unos novi)
+        {
+            List<Unos> sviUnosi = FileManager.Instance.ucitajUnose(putanja);
+            if (sviUnosi == null || index > sviUnosi.Count)
+                return;
+
+           sviUnosi[index] = novi;
+          
+            try
+            {
+                using FileStream fs = new FileStream(putanja, FileMode.Create, FileAccess.Write);
+                JsonSerializer.Serialize(fs, sviUnosi, _jsonOptions);
+            }
+            catch (Exception x)
+            {
+                Debug.WriteLine(x.Message);
+            }
+           
+        }
+
+        internal void obrisiUnos(string putanja, int index)
+        {
+            List<Unos> sviUnosi = FileManager.Instance.ucitajUnose(putanja);
+            if (sviUnosi == null || index > sviUnosi.Count)
+                return;
+            sviUnosi.RemoveAt(index);
+            try
+            {
+                using FileStream fs = new FileStream(putanja, FileMode.Create, FileAccess.Write);
+                JsonSerializer.Serialize(fs, sviUnosi, _jsonOptions);
+            }
+            catch (Exception x)
+            {
+                Debug.WriteLine(x.Message);
+            }
+        }
     }
 }
--- UI.cs
+++ UI.cs
@@ -1,13 +1,12 @@
 ﻿using journal.Model;
-using MessagePack;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using Terminal.Gui;
 
 namespace journal
 {
-
     public class UI
     {
         private static UI instance;
@@ -21,45 +20,51 @@ namespace journal
             }
         }
 
-
         string folder;
         public string putanja;
         static ColorScheme OsnovnaShema;
         static ColorScheme unosiShema;
         static ColorScheme polje;
 
+        ListView unosi;
+        TextField datumField;
+        TextField raspolozenjeField;
+        TextView desavanjaView;
+        TextView misaoView;
+        TextView idejeView;
+
         public void setup()
         {
             folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             if (!Directory.Exists(folder))
                 Directory.CreateDirectory(folder);
-            putanja = Path.Combine(folder, "dnevnik.enc");
+            putanja = Path.Combine(folder, "dnevnik.json");
 
             OsnovnaShema = new ColorScheme()
             {
                 Normal = Application.Driver.MakeAttribute(Color.White, Color.Black),
                 Focus = Application.Driver.MakeAttribute(Color.Black, Color.White)
             };
-
             unosiShema = new ColorScheme()
             {
                 Normal = Application.Driver.MakeAttribute(Color.White, Color.Black),
-                Focus = Application.Driver.MakeAttribute(Color.Black, Color.DarkGray)
+                Focus = Application.Driver.MakeAttribute(Color.Black, Color.DarkGray),
+                HotNormal = Application.Driver.MakeAttribute(Color.Black, Color.DarkGray),
+                HotFocus = Application.Driver.MakeAttribute(Color.Black, Color.DarkGray)
             };
-
             polje = new ColorScheme()
             {
                 Normal = Application.Driver.MakeAttribute(Color.White, Color.Black),
                 Focus = Application.Driver.MakeAttribute(Color.Gray, Color.Black)
             };
         }
+
+        List<string> skr = new();
         public void _kostur()
         {
-           
-            
             var top = Application.Top;
 
-            var win = new Window("Дневњак.")
+            var win = new Window("Dневњаk.")
             {
                 X = 0,
                 Y = 0,
@@ -68,58 +73,205 @@ namespace journal
                 ColorScheme = OsnovnaShema
             };
             top.Add(win);
-            List<string> skr = FileManager.Instance.skraceniUnosi(putanja);
-            
-            var unosi = new ListView()
+
+            skr = FileManager.Instance.skraceniUnosi(putanja);
+
+            var unosiOkvir = new FrameView("Unosi")
+            {
+                X = 1,
+                Y = 1,
+                Width = 32,
+                Height = Dim.Fill(),
+           
+            };
+            unosi = new ListView()
             {
                 X = 0,
                 Y = 0,
-                Width = 30,
+                Width = Dim.Fill(),
                 Height = Dim.Fill(),
                 ColorScheme = unosiShema
             };
             if (skr != null)
-                unosi = new ListView(skr);
+                unosi.SetSource(skr);
 
-            win.Add(unosi);
-
+            unosiOkvir.Add(unosi);
+            win.Add(unosiOkvir);
 
             var okvir = new FrameView()
             {
-                X = Pos.Right(unosi) + 1,
+                X = Pos.Right(unosiOkvir) + 1, 
                 Y = 0,
                 Width = Dim.Fill(),
                 Height = Dim.Fill(),
-
             };
             win.Add(okvir);
 
-            var detalji = new TextView()
+            var datumLabel = new Label("Datum:")
+            {
+                X = 0,
+                Y = 0
+            };
+            datumField = new TextField("")
+            {
+                X = Pos.Right(datumLabel) + 1,
+                Y = 0,
+                Width = 12,
+                ColorScheme = polje
+            };
+
+            var raspolozenjeLabel = new Label("Raspolozenje:")
+            {
+                X = Pos.Right(datumField) + 3,
+                Y = 0
+            };
+            raspolozenjeField = new TextField("")
+            {
+                X = Pos.Right(raspolozenjeLabel) + 1,
+                Y = 0,
+                Width = 3,
+                ColorScheme = polje
+            };
+
+            okvir.Add(datumLabel, datumField, raspolozenjeLabel, raspolozenjeField);
+
+            var desavanjaOkvir = new FrameView("Desavanja")
+            {
+                X = 0,
+                Y = 2,
+                Width = Dim.Fill(),
+                Height = Dim.Percent(45),
+            };
+            desavanjaView = new TextView()
             {
                 X = 0,
                 Y = 0,
                 Width = Dim.Fill(),
                 Height = Dim.Fill(),
-             
-
-                Text = "Datum: 2026-03-15\nMood: 5\nDesavanja:\nVibe-codeovao ovaj dnevnik. dodata enkripcija AES256. Valjda je dobro\n\nMisao:\n\nIdeje:",
+                WordWrap = true,
                 ColorScheme = polje
             };
-            okvir.Add(detalji);
+            desavanjaOkvir.Add(desavanjaView);
+            okvir.Add(desavanjaOkvir);
+
+            var misaoOkvir = new FrameView("Misao")
+            {
+                X = 0,
+                Y = Pos.Bottom(desavanjaOkvir),
+                Width = Dim.Fill(),
+                Height = Dim.Percent(25),
+            };
+            misaoView = new TextView()
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+                WordWrap = true,
+                ColorScheme = polje
+            };
+            misaoOkvir.Add(misaoView);
+            okvir.Add(misaoOkvir);
+
+            var idejeOkvir = new FrameView("Ideje")
+            {
+                X = 0,
+                Y = Pos.Bottom(misaoOkvir),
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+            };
+            idejeView = new TextView()
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+                WordWrap = true,
+                ColorScheme = polje
+            };
+            idejeOkvir.Add(idejeView);
+            okvir.Add(idejeOkvir);
+
+            prikaziUnos(0);
+
+            unosi.SelectedItemChanged += (args) =>
+            {
+                prikaziUnos(args.Item);
+                
+            };
 
             var footer = new StatusBar(new StatusItem[]
             {
-                new StatusItem(Key.F1, "~F1~ Novi", () => {  }),
-                new StatusItem(Key.F2, "~F2~ Sacuvaj", () => {  }),
-                new StatusItem(Key.F3, "~F3~ Obrisi", () => {  })
+                new StatusItem(Key.F1, "~F1~ Novi", Novi),
+                new StatusItem(Key.CtrlMask | Key.S, "~CTRL+S~ Sacuvaj", Sacuvaj),
+                new StatusItem(Key.CtrlMask | Key.D, "~CTRL+D~ Obrisi", Obrisi)
             })
             {
                 ColorScheme = OsnovnaShema
             };
             top.Add(footer);
-
-          
         }
-      
+        void Novi()
+        {
+            Unos novi = new Unos();
+            novi.datum = DateTime.Now;
+            novi.raspolozenje = 3;
+            novi.desavanja = string.Empty;
+            novi.misao = string.Empty;
+            novi.ideje = string.Empty;
+
+            FileManager.Instance.upisiUnos(putanja, novi); 
+            skr = FileManager.Instance.skraceniUnosi(putanja);
+            unosi.SetSource(skr);
+            unosi.SelectedItem = 0; 
+            prikaziUnos(0);
+        }
+        void prikaziUnos(int index)
+        {
+            List<Unos> svi = FileManager.Instance.ucitajUnose(putanja);
+            if (svi == null || index < 0 || index >= svi.Count)
+                return;
+
+            Unos izabrani = svi[index];
+            datumField.Text = izabrani.datum.ToString("yyyy-MM-dd");
+            raspolozenjeField.Text = izabrani.raspolozenje.ToString();
+            desavanjaView.Text = izabrani.desavanja ?? string.Empty;
+            misaoView.Text = izabrani.misao ?? string.Empty;
+            idejeView.Text = izabrani.ideje ?? string.Empty;
+        }
+        void Obrisi()
+        {
+            int izabrani = unosi.SelectedItem;
+            List<Unos> svi = FileManager.Instance.ucitajUnose(putanja);
+            if (svi == null || izabrani < 0 || izabrani >= svi.Count)
+                return;
+            FileManager.Instance.obrisiUnos(putanja,izabrani);
+            skr = FileManager.Instance.skraceniUnosi(putanja);
+            unosi.SetSource(skr);
+            prikaziUnos(izabrani-1);
+
+        }
+        void Sacuvaj()
+        {
+            Unos noviUnos = new Unos();
+            if (DateTime.TryParse(datumField.Text.ToString(), out DateTime datum))
+                noviUnos.datum = datum;
+            else
+                noviUnos.datum = DateTime.Now;
+
+            if (int.TryParse(raspolozenjeField.Text.ToString(), out int raspolozenje))
+                noviUnos.raspolozenje = raspolozenje;
+            else
+                noviUnos.raspolozenje = 3;
+
+            noviUnos.desavanja = desavanjaView.Text.ToString();
+            noviUnos.misao = misaoView.Text.ToString();
+            noviUnos.ideje = idejeView.Text.ToString();
+
+            FileManager.Instance.sacuvajUnos(putanja, unosi.SelectedItem, noviUnos);
+            skr = FileManager.Instance.skraceniUnosi(putanja);
+            unosi.SetSource(skr);
+        }
+     
     }
 }
